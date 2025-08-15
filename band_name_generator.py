011b37d8848d3a1241ab38f2418eb63c7d1a4a9a
wables <99419627+w-ables@users.noreply.github.com>
2025-08-14 22:24:03 -0500

    Update band_name_generator.py
    added code

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator!")
+
+city = input("What's the name of your city? ")
+pet = input("What's the name of your pet? ")
+
+band_name = f"{city} {pet}"
+print(f"Your band name could be: {band_name}")
